31186809bb3dc35b6db8bea6a2e334837b2bf0c1
harshithadale <daleharshi6045@gmail.com>
2025-07-18 16:51:39 +0530

    third-commit

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,19 +1,44 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-function UserDetails({users}) {
-    const {id} = useParams()
-    const user = users.find(item=>item.id === Number(id))
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function UserDetails({ users }) {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+
+  const user = users.find((item) => item.id === Number(id))
+  useEffect(()=>{
+    const timeout = setTimeout(()=>{
+        setLoading(false)
+    },1000)
+    return ()=>clearTimeout(timeout)
+  },[])
+
   return (
     <div>
-        { console.log(user)}
-        <h1>User Details</h1>
-        <p><b>Name:</b>{user.name}</p>
-        <p><b>Name:</b>{user.username}</p>
-        <p><b>Name:</b>{user.email}</p>
-        <p><b>Name:</b>{user.phone}</p>
-        <p><b>Name:</b>{user.website}</p>
+      {loading ? (
+        "loading..."
+      ) : (
+        <>
+          <h1>User Details</h1>
+          <p>
+            <b>Name:</b> {user.name}
+          </p>
+          <p>
+            <b>Username:</b> {user.username}
+          </p>
+          <p>
+            <b>Email:</b> {user.email}
+          </p>
+          <p>
+            <b>Phone:</b> {user.phone}
+          </p>
+          <p>
+            <b>Website:</b> {user.website}
+          </p>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default UserDetails
+export default UserDetails;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
-function UserList({users}) {
+import { Link, Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+function UserList({ users }) {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>User List</h1>
       {users.length === 0
-        ? "loading"
+        ? "loading..."
         : users.map((item, ind) => {
             return (
               <ul>
-                <Link to={`/users/${item.id}`}>
+                <a
+                  href={`/users/${item.id}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate(`/users/${item.id}`);
+                  }}
+                >
                   <li>{item.name}</li>
-                </Link>
+                </a>
               </ul>
             );
           })}
